653f024f4c110ca894ad363df4774efc2ca40e41
anttisiika <antti.siika@ki.se>
2023-11-29 14:26:14 +0100

    document()

--- R/prettify_colnames.R
+++ R/prettify_colnames.R
@@ -1,4 +1,16 @@
-
+#' Prettifies colnames
+#'
+#' This function prettifies colnames for quick plotting/tables
+#' removes _ and makes space instead, makes titlecase and removes
+#' an optional prefix.
+#'
+#'  @param df input dataframe
+#'  @prefix prefix to remove is set to remove processed 'pr_'
+#'
+#'  @export
+#'  @examples
+#'  prettify_colnames(df, prefix = 'hej)
+#'
 prettify_colnames <- function(df, prefix = 'pr_'){
   df %>%
     dplyr::rename_with(., ~stringr::str_replace_all(., prefix, ' ')) %>%
