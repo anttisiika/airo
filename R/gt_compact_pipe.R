6cffe20788a7300ffd7ad66195560e2f45b9296d
anttisiika <antti.siika@ki.se>
2023-12-01 11:15:44 +0100

    compact tbl

---
+++ R/gt_compact_pipe.R
@@ -0,0 +1,21 @@
+
+#' Compact pipe to gt table
+#' also makes font roboto..
+#' @param data
+#'
+#' @return gt
+#' @export
+#'
+#'
+gt_compact_pipe <- function(data){
+gt::tab_options(table.font.names = "Roboto Condensed") %>%
+
+  gt::tab_options(
+    data_row.padding = gt::px(1),
+    summary_row.padding = gt::px(1),
+    grand_summary_row.padding = gt::px(1),
+    footnotes.padding = gt::px(1),
+    source_notes.padding = gt::px(1),
+    row_group.padding = gt::px(1)
+  )
+}
